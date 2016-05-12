50663e4c8b25ddaa4496c6476c0095fedbc808cc
teamZeta <bohtebostjan2@gmail.com>
2016-05-12 13:48:35 +0200

    clock

--- src/tournament3/src/findFaces.cpp
+++ src/tournament3/src/findFaces.cpp
@@ -8,19 +8,39 @@
 #include <math.h> 
 #include <std_msgs/String.h>
 #include <sstream>
-#include <ctime>
+#include <cstdlib>
+#include <sys/timeb.h>
 using namespace std;
 
+
+static int start;
+static int getMilliCount(){
+	timeb tb;
+	ftime(&tb);
+	int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+	return nCount;
+}
+
+static int getMilliSpan(int nTimeStart){
+	int nSpan = getMilliCount() - nTimeStart;
+	if(nSpan < 0)
+		nSpan += 0x100000 * 1000;
+	return nSpan;
+}
+
+
 void callback (const visualization_msgs::MarkerArrayConstPtr& markerArray) {
+	
 	printf("Sem v callback in imam -- %d -- obrazov \n", (int)markerArray->markers.size());
-	cout<<"Time Difference: "<<clock()<<endl;
+	int milliSecondsElapsed = getMilliSpan(start);
+	printf("\n\nElapsed time = %u milliseconds", milliSecondsElapsed);
 	
 }
 
 int main(int argc, char** argv){
   	ros::init(argc, argv, "findFaces");
 	ros::NodeHandle nh;
-	
+	start = getMilliCount();
   	ros::Subscriber sub = nh.subscribe<visualization_msgs::MarkerArray> ("/markers", 100, callback);	
 	ros::spin();
 }
