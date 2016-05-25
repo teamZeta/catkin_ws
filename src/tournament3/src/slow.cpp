f115a11372c30a084a816a0e99ddb9dde404ea5a
boka <bohtebostjan@gmail.com>
2016-05-25 19:00:10 +0200

    znaki

---
+++ src/tournament3/src/slow.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/String.h>
+#include <sstream>
+#include <cstdlib>
+#include <sys/timeb.h>
+#include <sound_play/sound_play.h>
+#include <unistd.h>
+
+using namespace std;
+
+void callback (const visualization_msgs::MarkerArrayConstPtr& markerArray) {
+    if (markerArray->markers[0].id == 1) {      // horn
+        ros::NodeHandle nh2;
+        //ros::Publisher pub = nh2.advertise<sound_play::SoundRequest>("/robotsound", 1);
+
+       // pub.publish(4);
+
+        std::string topic = "/robotsound";
+        sound_play::SoundClient sc(nh2,topic);
+        sc.play(sound_play::SoundRequest::NEEDS_UNPLUGGING_BADLY);
+    }
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "traffic");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe<visualization_msgs::MarkerArray> ("/sign", 1, callback);
+
+    ros::spin();
+
+    //return 0;
+}
