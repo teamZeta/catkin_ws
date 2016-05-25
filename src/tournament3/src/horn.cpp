12306e33dd375e4f52387915a89dd97aefb9c924
boka <bohtebostjan@gmail.com>
2016-05-25 16:00:55 +0200

    dodan horn

---
+++ src/tournament3/src/horn.cpp
@@ -0,0 +1,40 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/String.h>
+#include <sstream>
+#include <cstdlib>
+#include <sys/timeb.h>
+#include <sound_play/sound_play.h>
+#include <unistd.h>
+
+using namespace std;
+
+void callback (const visualization_msgs::MarkerArrayConstPtr& markerArray) {
+    if (markerArray->markers[0].id == 1) {      // horn
+        ros::NodeHandle nh2;
+        sound_play::SoundClient sc;
+        if(nh2.ok()){
+            sleep(1);
+            sound_play::Sound s3 = sc.builtinSound(sound_play::SoundRequest::NEEDS_UNPLUGGING_BADLY);
+            s3.play();
+            sleep(15);
+            s3.stop();
+        }
+    }
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "horn");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe<visualization_msgs::MarkerArray> ("/sign", 1, callback);
+
+    ros::spin();
+    
+    //return 0;
+}
