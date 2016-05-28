e8b1075d0802646bcb7a432fc6565a4cc4932cf1
team zeta <bohtebostjan@gmail.com>
2016-05-28 16:46:15 +0200

    teleport

--- src/tournament3/src/teleport.cpp
+++ src/tournament3/src/teleport.cpp
@@ -12,7 +12,7 @@
 #include <sound_play/sound_play.h>
 #include <unistd.h>
 #include <std_msgs/Header.h>
-#include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 using namespace std;
 
@@ -24,27 +24,44 @@ void callback (const visualization_msgs::MarkerArrayConstPtr& markerArray) {
     }
 }
 
+void callback2(const geometry_msgs::PoseWithCovarianceStamped msg){
+    //msg.pose.pose.position.y+=110;
+    geometry_msgs::PoseWithCovarianceStamped pos;
+    pos.pose.pose.position.x = msg.pose.pose.position.x;
+    pos.pose.pose.position.y = msg.pose.pose.position.y+110;
+    pos.pose.pose.position.z = msg.pose.pose.position.z;
+    pos.pose.pose.orientation.x = msg.pose.pose.orientation.x;
+    pos.pose.pose.orientation.y = msg.pose.pose.orientation.y;
+    pos.pose.pose.orientation.z = msg.pose.pose.orientation.z;
+    pos.pose.pose.orientation.w = msg.pose.pose.orientation.w;
+
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1);
+    pub.publish(pos);
+}
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "oneway");
     ros::NodeHandle nh;
 
     ros::Subscriber sub = nh.subscribe<visualization_msgs::MarkerArray> ("/sign", 1, callback);
     ////////////////////
-
-    tf::TransformListener listener(nh,ros::Duration(5),true);
-    tf::StampedTransform transform;
-    listener.waitForTransform("/map", "/base_link", ros::Time::now(), ros::Duration(10));
-    listener.lookupTransform("/map", "/base_link", ros::Time::now(), transform);
+    ros::NodeHandle nh2;
+    ros::Subscriber sub2 = nh2.subscribe<geometry_msgs::PoseWithCovarianceStamped> ("/amcl_pose", 1, callback2);
 
 
-    ros::Pose robotPos = transform.getOrigin().getPosition();
-    robotPos.point.y+=110;
+    /*geometry_msgs::PoseWithCovarianceStamped pos;
+    pos.pose.pose.position.x = transform.getOrigin().x();
+    pos.pose.pose.position.y = transform.getOrigin().y()+110;
+    pos.pose.pose.position.z = transform.getOrigin().z();
+    pos.pose.pose.orientation.x = transform.getRotation().x();
+    pos.pose.pose.orientation.y = transform.getRotation().y();
+    pos.pose.pose.orientation.z = transform.getRotation().z();
+    pos.pose.pose.orientation.w = transform.getRotation().w();
+    //position.pose.position.y+=110;
 
-    ros::geometry_msgs::PoseWithCovarianceStamped position;
-    position.pose = robotPos;
-
-    pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/amcl/initialpose", 1);
-    pub.publish(position);
+    ros::Publisher pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 1);
+    pub.publish(pos);*/
 
 
 
