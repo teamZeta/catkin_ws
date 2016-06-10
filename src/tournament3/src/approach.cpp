65213bbf1c5e11867f2ecb99eec8ada76c095a28
teamZeta <teamZeta@github.com>
2016-06-10 13:26:03 +0200

    aproach malo poravljen kao2

--- src/tournament3/src/approach.cpp
+++ src/tournament3/src/approach.cpp
@@ -314,11 +314,6 @@ void callbackTalk (const std_msgs::String::ConstPtr& msg1) {
     //printf("TAXI: dobil sem msg\n");
     printf("Dobil sem %s\n",msg1->data.c_str());
     if (!strcmp(msg1->data.c_str(),"pathEnded") && found){
-        
-        iskanHotelID = 0;
-        isciHotel = false;
-        iskanaOsebaID = 0;
-        isciOsebo = false;
 
         MoveBaseClient ac("move_base", true);
         //wait for the action server to come up
@@ -367,6 +362,10 @@ void callbackTalk (const std_msgs::String::ConstPtr& msg1) {
 
         }
         found = false;
+        iskanHotelID = 0;
+        isciHotel = false;
+        iskanaOsebaID = 0;
+        isciOsebo = false;
     }
 }
 
