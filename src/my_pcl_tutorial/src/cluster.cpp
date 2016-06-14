6c7f51b4f4a9b3d05c835f6c926f382be1716dbc
team zeta <bohtebostjan@gmail.com>
2016-06-14 12:54:40 +0200

    brez plane

--- src/my_pcl_tutorial/src/cluster.cpp
+++ src/my_pcl_tutorial/src/cluster.cpp
@@ -217,7 +217,7 @@ static void mark_cluster(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster, s
   }*/
   	int barva = 0;
   
-  	if(hsv_barva.h > 90 && hsv_barva.h < 140 && hsv_barva.s > -1){
+  	if(hsv_barva.h > 75 && hsv_barva.h < 140 && hsv_barva.s > -1){
 	  	g = 1;
 	  	r = 0;
 	  	b = 0;
@@ -237,7 +237,7 @@ static void mark_cluster(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster, s
 	  	b = 0;
 	  	ru = 0xff;
 	  	barva = 1;
-	  }else if(hsv_barva.h > 30 && hsv_barva.h < 80 && hsv_barva.s > -1){
+	  }else if(hsv_barva.h > 30 && hsv_barva.h < 70 && hsv_barva.s > -1){
       g = 1;
       r = 1;
       b = 0;
